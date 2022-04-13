67a542b12d9948b148fc98c1e9f867a19d6aaf21
sam narayan <66398171+sam-narayan@users.noreply.github.com>
2022-04-14 10:56:19 +1200

    added the order form

--- pizza_delivery_real.py
+++ pizza_delivery_real.py
@@ -17,6 +17,11 @@ buttons.grid(row=1, column=0)
 label_text = StringVar()
 label_text.set('pickup or delivery?!')
 name2_frame = Frame(window)
+master = Frame(window)
+
+def show():
+    global current_order
+    current_order.set("order: \nham: {} \nfemale: {} \ncheese: {} \nname: {}".format(var1.get(), var2.get(), var3.get(), name2.get()))
 
 def dcmd():
     forget(buttons)
@@ -58,6 +63,8 @@ def pickcmd():
 
 def name2cmd():
     forget(name2_frame)
+    add_widget(master, 1)
+    label_text.set('order?')
 
 top_text = Label(top, textvariable = label_text)
 top_text.pack()
@@ -68,32 +75,45 @@ delivery_button.grid(row=0, column=0, padx=10, pady=50)
 pickup_button = Button(buttons, text='pick up', command=pickcmd)
 add_widget(pickup_button, 0, 1, 10, 50)
 
-name = DoubleVar()
+name = StringVar()
 name.set('')
 
 name_box= Entry(detail_frame, textvariable= name)
 
 confirmbutton= Button(detail_frame, text='confirm', command=confirm)
 
-name2 = DoubleVar()
+name2 = StringVar()
 name2.set('')
 
 name2box= Entry (name2_frame, textvariable= name2)
 
 name2confirm= Button(name2_frame, text='confirm', command=name2cmd)
 
-address = DoubleVar()
+address = StringVar()
 address.set('')
 
 address_box= Entry(address_frame, textvariable= address )
 
 address_confirm= Button(address_frame, text='confirm', command=acmd)
 
-number= DoubleVar()
+number= StringVar()
 number.set('')
 
 number_box= Entry(phone_frame, textvariable= number)
 
 number_confirm= Button(phone_frame, text='confirm', command=pcmd)
+
+current_order= StringVar()
+current_order.set('')
+Label(master, text="toppings:").grid(row=0, sticky=W)
+var1 = IntVar()
+Checkbutton(master, text="ham", variable=var1).grid(row=1, sticky=W)
+var2 = IntVar()
+Checkbutton(master, text="female", variable=var2).grid(row=2, sticky=W)
+var3 = IntVar()
+Checkbutton(master, text="cheese", variable=var3).grid(row=3, sticky=W)
+Button(master, text='cancel', command=master.quit).grid(row=4, sticky=W, pady=4)
+Button(master, text='Show', command=show).grid(row=5, sticky=W, pady=4)
+Label(master, textvariable=current_order).grid(row=6, sticky=W, pady=4)
 
 window.mainloop()
